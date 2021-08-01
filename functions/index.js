cbed8b21147bec974700d3f37a4fe7d8e509ab95
Iryna Borniak <la.aerea.1@gmail.com>
2021-08-02 00:40:21 +0300

    Button position fixed for mobile screens

--- functions/index.js
+++ functions/index.js
@@ -22,10 +22,3 @@ exports.sendContactMessage = functions.database.ref('/messages/{pushId}').onCrea
   ).catch(error => console.log(error));
 });
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
